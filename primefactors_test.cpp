0c10498b4aab4f875aa39347824f2cc68cd95bab
Kwangwon Min <kwangwon.min@gmail.com>
2025-07-02 10:00:49 +0900

    [feature] page 45

--- primefactors_test.cpp
+++ primefactors_test.cpp
@@ -5,5 +5,10 @@ TEST(PrimeFactors, Of1) {
 	PrimeFactors prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
-	EXPECT_EQ(1, 1);
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
