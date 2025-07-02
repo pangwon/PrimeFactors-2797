f506a147d33d23382e96dcd323bb4875bde588d2
Kwangwon Min <kwangwon.min@gmail.com>
2025-07-02 09:58:16 +0900

    [feature] page 41

--- primefactors_test.cpp
+++ primefactors_test.cpp
@@ -1,6 +1,9 @@
 #include "gmock/gmock.h"
 #include "primefactors.cpp"
 
-TEST(PrimeFactors, PrimeTest) {
+TEST(PrimeFactors, Of1) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 	EXPECT_EQ(1, 1);
 }
